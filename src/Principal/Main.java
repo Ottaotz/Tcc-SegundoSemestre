8221d00d6426013523194e666706c6188e2cb265
ottosschmitz <ottosschmitz@gmail.com>
2022-11-21 15:32:13 -0300

    Add tests

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,13 +1,13 @@
-package Principal;
-
-import Tela.UI;
-
-public class Main {
-
-	public static void main(String[] args) {
-		UI ui = new UI();
-		ui.loopMenu();
-
-	}
-
-}
+//package Principal;
+//
+//import Tela.UI;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		UI ui = new UI();
+//		ui.loopMenu();
+//
+//	}
+//
+//}
